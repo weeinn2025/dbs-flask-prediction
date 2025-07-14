f80c013b27d06f201edfcb056146f2a5e76ae3fa
weeinn2025 <weeinn.danotes@gmail.com>
2025-07-14 16:44:31 +0800

    1

--- app.py
+++ app.py
@@ -1,35 +1,55 @@
 from flask import Flask, render_template, request
-#import os
 import joblib
+from groq import Groq
+
+import os
+os.environ['GROQ_API_KEY'] = " "
+# for cloud ..........
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
 
-## Route to handle the main page logic
-@app.route("/main", methods=["GET", "POST"])
+@app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
-    #db
-    return render_template("main.html")
+    # db
+    return(render_template("main.html"))
 
-## Route to handle the DBS prediction page
-@app.route("/dbs", methods=["GET", "POST"])
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+    # load model
+    client = Groq()
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html",r=completion.choices[0].message.content))
+
+@app.route("/dbs",methods=["GET","POST"])
 def dbs():
-    return render_template("dbs.html")
+    return(render_template("dbs.html"))
 
-## Route to handle the prediction logic for basic prediction
-@app.route("/prediction", methods=["GET","POST"])
+@app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
     # load model
     model = joblib.load("dbs.jl")
     # make prediction
-    #prediction_result = (-50.6 * q) + 90.2
     pred = model.predict([[q]])
-    return render_template("prediction.html", r=prediction_result)
+    return(render_template("prediction.html",r=pred))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run()
